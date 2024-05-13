7bd607329ead2791c45a9148930c964ac927defd
Joaoaugustohm <142924955+Joaoaugustohm@users.noreply.github.com>
2024-05-13 08:03:12 -0300

    att Mastro

---
+++ mastros/script.js
@@ -0,0 +1,14 @@
+function calcularOrcamento() {
+  // Obter os valores dos campos de entrada
+  const nome = document.getElementById('nome').value;
+  const metrosQuadrados = parseFloat(document.getElementById('metrosQuadrados').value);
+  const preco = parseFloat(document.getElementById('preco').value);
+
+  // Calcular o custo total
+  const custoTotal = ((metrosQuadrados * preco) * 1.25) * 1.62;
+  // Exibir o resultado na página
+  const resultadoElement = document.getElementById('resultado');
+  resultadoElement.textContent = `Nome do Produto: ${nome}\nMetros Quadrados: ${metrosQuadrados} 
+  m²\nCusto Total: R$ ${custoTotal.toFixed(2)}`;
+}
+
